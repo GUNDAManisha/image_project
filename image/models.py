0ab819f1d15db9cd0e455a75b551dd36e20c0f83
Manisha <gundamanisha752@gmail.com>
2021-06-07 16:34:34 +0530

    image project

---
+++ image/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30,null=False,blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images")
+    description = models.TextField()
+
